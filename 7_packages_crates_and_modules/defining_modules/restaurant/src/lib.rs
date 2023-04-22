7988d3db38ef882756ad48f3ac450bad926c8434
adibfarrasy <muhammad.farrasy@agriaku.com>
2023-04-22 23:16:56 +0700

    Added ch.7

---
+++ 7_packages_crates_and_modules/defining_modules/restaurant/src/lib.rs
@@ -0,0 +1,12 @@
+#[allow(dead_code)]
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+    mod serving {
+        fn take_order() {}
+        fn server_order() {}
+        fn take_payment() {}
+    }
+}
